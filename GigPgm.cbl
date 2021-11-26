9126d1b0f75eeb8d8d3c60ac01f3d9158964485f
Danuszek <93706183+Danuszek@users.noreply.github.com>
2021-11-26 13:03:48 +0100

    Add files via upload

---
+++ GigPgm.cbl
@@ -0,0 +1,6 @@
+       IDENTIFICATION DIVISION.
+       PROGRAM-ID.     GITPGM.
+   
+       PROCEDURE DIVISION.
+           DISPLAY "This is my Git pgm stored in LocalRepositories".
+           STOP RUN.
