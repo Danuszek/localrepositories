54d63c2236e1b0d3dd6a9aaba9035382ac0f19fd
Danuszek <93706183+Danuszek@users.noreply.github.com>
2022-08-17 15:35:54 +0200

    Update Japcob1.cbl

--- Japcob1.cbl
+++ Japcob1.cbl
@@ -2,10 +2,10 @@
        PROGRAM-ID.  JAPCOB1.
        ENVIRONMENT DIVISION.
        CONFIGURATION SECTION.
-      *ありがとうございます
        DATA DIVISION.
        WORKING-STORAGE SECTION.
        77  AA PIC X VALUE "ｿ".
+       77  ZZ PIC X(50) VALUE "ありがとうございます".
        77  BB PIC X.
 
        PROCEDURE DIVISION.
