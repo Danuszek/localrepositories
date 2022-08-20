e6012e864b4f21d4190b5916d30ee116321e04fe
Danuszek <93706183+Danuszek@users.noreply.github.com>
2022-08-20 16:25:00 +0200

    Update Japcob1.cbl

--- Japcob1.cbl
+++ Japcob1.cbl
@@ -7,7 +7,8 @@
        77  AA PIC X VALUE "ｿ".
        77  ZZ PIC X(50) VALUE "ありがとうございます".
        77  BB PIC X.
-
+      * ｿ
+      *ありがとうございます
        PROCEDURE DIVISION.
            MOVE BB TO AA.
            GOBACK.
